7ee6e1f4a7075b97dbdbb984e0359595165f8cf9
Laura Portillo <lauraportillo.ark@gmail.com>
2021-07-18 00:56:41 +0200

    add home component

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,24 @@
 // import React, { useState } from 'react';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+
 // Components
+import Home from './Home';
 // import Header from './Header';
 // import Footer from './Footer';
+
 // Styles
 import '../stylesheets/App.scss';
 import '../stylesheets/Reset.scss';
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  return (
+    <div className="containerBody">
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../stylesheets/Home.scss';
+
+const Home = (props) => {
+  return (
+    <article>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <h1>free rocky</h1>
+      <button></button>
+    </article>
+  );
+};
+
+export default Home;
