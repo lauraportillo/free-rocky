ec06f2c1f0c2726714e306885bcec6d7e82b899a
Laura Portillo <lauraportillo.ark@gmail.com>
2021-07-23 16:53:08 +0200

    add component clue01

---
+++ src/components/Clue01.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../stylesheets/Clue01.scss';
+
+const Clue01 = (props) => {
+  return (
+    <>
+      <div>
+        <h1>primera hoja del juego</h1>
+      </div>
+    </>
+  );
+};
+
+export default Clue01;
