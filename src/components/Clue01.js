b9a761a6abd235f22f247aadb99e9aed7933cdd1
Laura Portillo <lauraportillo.ark@gmail.com>
2021-07-25 16:55:31 +0200

    add start structure

--- src/components/Clue01.js
+++ src/components/Clue01.js
@@ -1,20 +1,20 @@
-// // React
-// import { Link } from 'react-router-dom';
-// // Styles
-// import '../stylesheets/Clue01.scss';
+// React
+import { Link } from 'react-router-dom';
+// Styles
+import '../stylesheets/Clue01.scss';
 
-// const Clue01 = (props) => {
-//   return (
-//     <>
-//       <div className="back">
-//         <h1>primera hoja del juego</h1>
+const Clue01 = (props) => {
+  return (
+    <>
+      <div className="back">
+        <h1>primera hoja del juego</h1>
 
-//         <h2>
-//           <Link to="/">go back</Link>
-//         </h2>
-//       </div>
-//     </>
-//   );
-// };
+        <h2>
+          <Link to="/">go back</Link>
+        </h2>
+      </div>
+    </>
+  );
+};
 
-// export default Clue01;
+export default Clue01;
--- src/components/Start.js
+++ src/components/Start.js
@@ -2,13 +2,24 @@
 import { Link } from 'react-router-dom';
 // Components
 import Clue01 from './Clue01';
+import Footer from './Footer';
 // Styles
 import '../stylesheets/Start.scss';
 
 const Start = (props) => {
   return (
     <>
-      <h1> COMPONENTE START</h1>
+      <header className="startHeader">
+        <h1 className="startHeader__title">free rocky!</h1>
+        <div>
+          <h2>time remaining</h2>
+          <div>00.00</div>
+        </div>
+      </header>
+      <main className="startMain">
+        <Clue01 />
+      </main>
+      <Footer />
     </>
   );
 };
