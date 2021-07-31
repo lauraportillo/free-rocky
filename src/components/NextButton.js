585ce358fe620dba57c3d0af698e9e8a6d7c2500
Laura Portillo <lauraportillo.ark@gmail.com>
2021-07-31 10:48:10 +0200

    add message coorect or incorrect

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -5,11 +5,30 @@ const NextButton = (props) => {
     props.handlePassword();
   };
 
+  const renderMessage = () => {
+    const password = 'clave';
+    if (props.clue01 === password) {
+      return (
+        <>
+          <h3 className="next">Correcto</h3>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <h3 className="next">Incorrecto</h3>
+        </>
+      );
+    }
+  };
+
   return (
     <>
       <button disabled={!props.enabled} onClick={handlePassword} className="next">
         Next
       </button>
+
+      <div className={`next ${props.hiddenClass}`}>{renderMessage()}</div>
     </>
   );
 };
