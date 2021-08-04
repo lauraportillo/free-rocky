a332c01f84f14679aff47ff2f758ab489a4f84b9
Laura Portillo <lauraportillo.ark@gmail.com>
2021-08-04 12:34:14 +0200

    complete component clue02 with form

--- src/components/Clue02.js
+++ src/components/Clue02.js
@@ -2,16 +2,36 @@
 import { Link } from 'react-router-dom';
 // Components
 import ClueHeader from './ClueHeader';
+import Input from './Input';
+import NextButton from './NextButton';
+import ResetButton from './ResetButton';
 import Footer from './Footer';
 // Styles
 import '../stylesheets/Clue02.scss';
 
 const Clue02 = (props) => {
+  const handleForm = (ev) => {
+    ev.preventDefault();
+  };
   return (
     <>
       <ClueHeader />
       <main className="clue02">
         <h1 className="clue02__title">segunda</h1>
+        <form onSubmit={handleForm} className="clue01__form">
+          <div>
+            <Input handleInput={props.handleInput} input={props.input} className="clue01__form--input" />
+            <ResetButton handleReset={props.handleReset} />
+          </div>
+
+          {/* <NextButton
+            handleInput={props.handleInput}
+            input={props.input}
+            password={props.password}
+            hiddenClass={props.hiddenClass}
+          /> */}
+        </form>
+
         <nav className="clue02Nav">
           <ul className="clue02Nav__menu">
             <li>
