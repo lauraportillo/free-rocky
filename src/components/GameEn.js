ebb2f3ef026cd4cb668557ca04a5bb6c90e10eb5
Laura Portillo <lauraportillo.ark@gmail.com>
2021-08-12 16:07:32 +0200

    adjust text

--- src/components/GameEn.js
+++ src/components/GameEn.js
@@ -1,15 +1,28 @@
+// React
+import { Link } from 'react-router-dom';
+
 // Components
 import Header from './Header';
 import Footer from './Footer';
 // Styles
 import '../stylesheets/Game.scss';
-
 const GameEn = (props) => {
   return (
     <>
       <Header />
       <main className="game">
         <h1 className="game__title">the game</h1>
+
+        <ul className="game__lang">
+          <li className="game__lang--en">
+            <Link to="/aboutEn">En</Link>
+          </li>
+          <li className="game__lang--item">|</li>
+          <li className="game__lang--es">
+            <Link to="/aboutEs">Es</Link>
+          </li>
+        </ul>
+
         <section className="game__description">
           <p className="game__description--p">
             Rocky is not a regular dog.It is a very special dog. It is a Bulldog with a beautiful face, like a cuddly
