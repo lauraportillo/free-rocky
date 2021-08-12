d858d50971960cde5eaf2efbfd6c0f339080fd98
Laura Portillo <lauraportillo.ark@gmail.com>
2021-08-12 15:00:09 +0200

    divide the paragraph in small pieces

--- src/components/GameEn.js
+++ src/components/GameEn.js
@@ -10,13 +10,24 @@ const GameEn = (props) => {
       <Header />
       <main className="game">
         <h1 className="game__title">the game</h1>
-        <p className="game__description">
-          Rocky is not a regular dog.It is a very special dog. It is a Bulldog with a beautiful face, like a cuddly
-          teddy bear. The perfect dog that everyone would want to live with. Rocky seems completely real, however he is
-          not flesh and blood, he is a 3d modelled dog, the work of the great artist Luis Yrisarry Labadía. Luis is a 3d
-          Artist and Art Director specialized in Characters and Digital Sculpture, with such an incredible talent. Thank
-          you Luis for lend me Rocky as protagonist of this great adventure! Luis Yrisarry Labadía
-        </p>
+        <section className="game__description">
+          <p className="game__description--p">
+            Rocky is not a regular dog.It is a very special dog. It is a Bulldog with a beautiful face, like a cuddly
+            teddy bear. The perfect dog that everyone would want to live with.
+          </p>
+          <p className="game__description--p">
+            Rocky seems completely real, however he is not flesh and blood, he is a 3d modelled dog, the work of the
+            great artist Luis Yrisarry Labadía.
+          </p>
+          <p className="game__description--p">
+            Luis is a 3d Artist and Art Director specialized in Characters and Digital Sculpture, with such an
+            incredible talent.
+          </p>
+          <p className="game__description--p">
+            Thank you Luis for lend me Rocky as protagonist of this great adventure!
+          </p>
+          Luis Yrisarry Labadía
+        </section>
       </main>
       <Footer />
     </>
