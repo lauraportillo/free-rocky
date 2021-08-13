8a0fcf984e055e6c49901b95ab8fcdd83b9612b2
Laura Portillo <lauraportillo.ark@gmail.com>
2021-08-14 01:38:06 +0200

    update

--- src/components/GameEs.js
+++ src/components/GameEs.js
@@ -29,15 +29,15 @@ const GameEs = (props) => {
         <section className="game__description">
           <p className="game__description--p">
             <span className="bold"> </span>
-            HE AÑADIDO YA LA CLAVE SSH En el año 2020, una pandemia mundial paralizó todo el planeta. Desde entonces,
-            los dueños de Rocky, por miedo a que se contagiara, le han encerrado en una casa abandonada cuya puerta se
-            abre con una clave secreta. Una vez al día, le traen comida y agua. Rocky es un perro divertido y
-            dicharachero al que le apasiona viajar por todo el mundo. Le encanta conocer nuevas culturas, hablar
-            diferentes idiomas, charlar con animales salvajes y convivir con humanos amigables… No soporta más este
-            encierro! Ayer compró unos billetes de avión para escapar de su cautiverio, lejos, muy lejos, pero no
-            consigue descifrar la clave secreta que abre esa puerta y el avión despega en unas horas. Necesita tu ayuda!
-            Tienes 60 minutos para averiguar todas las pistas que te llevarán a descubrir la clave, y abrir la puerta
-            para que Rocky pueda escapar. Free Rocky!
+            En el año 2020, una pandemia mundial paralizó todo el planeta. Desde entonces, los dueños de Rocky, por
+            miedo a que se contagiara, le han encerrado en una casa abandonada cuya puerta se abre con una clave
+            secreta. Una vez al día, le traen comida y agua. Rocky es un perro divertido y dicharachero al que le
+            apasiona viajar por todo el mundo. Le encanta conocer nuevas culturas, hablar diferentes idiomas, charlar
+            con animales salvajes y convivir con humanos amigables… No soporta más este encierro! Ayer compró unos
+            billetes de avión para escapar de su cautiverio, lejos, muy lejos, pero no consigue descifrar la clave
+            secreta que abre esa puerta y el avión despega en unas horas. Necesita tu ayuda! Tienes 60 minutos para
+            averiguar todas las pistas que te llevarán a descubrir la clave, y abrir la puerta para que Rocky pueda
+            escapar. Free Rocky!
           </p>
         </section>
       </main>
