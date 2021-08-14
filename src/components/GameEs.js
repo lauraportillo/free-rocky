47b84cb26ec1a568a7f62a998cf8b6fc28a1bf2d
Laura Portillo <lauraportillo.ark@gmail.com>
2021-08-14 15:43:24 +0200

    add spanish text to compo game es

--- src/components/GameEs.js
+++ src/components/GameEs.js
@@ -28,17 +28,26 @@ const GameEs = (props) => {
 
         <section className="game__description">
           <p className="game__description--p">
-            <span className="bold"> </span>
             En el año 2020, una pandemia mundial paralizó todo el planeta. Desde entonces, los dueños de Rocky, por
             miedo a que se contagiara, le han encerrado en una casa abandonada cuya puerta se abre con una clave
-            secreta. Una vez al día, le traen comida y agua. Rocky es un perro divertido y dicharachero al que le
-            apasiona viajar por todo el mundo. Le encanta conocer nuevas culturas, hablar diferentes idiomas, charlar
-            con animales salvajes y convivir con humanos amigables… No soporta más este encierro! Ayer compró unos
-            billetes de avión para escapar de su cautiverio, lejos, muy lejos, pero no consigue descifrar la clave
-            secreta que abre esa puerta y el avión despega en unas horas. Necesita tu ayuda! Tienes 60 minutos para
-            averiguar todas las pistas que te llevarán a descubrir la clave, y abrir la puerta para que Rocky pueda
-            escapar. Free Rocky!
+            secreta.
           </p>
+          <p className="game__description--p">Una vez al día, le traen comida y agua.</p>
+          <p className="game__description--p">
+            Rocky es un perro divertido y dicharachero al que le apasiona viajar por todo el mundo. Le encanta conocer
+            nuevas culturas, hablar diferentes idiomas, charlar con animales salvajes y convivir con humanos amigables…
+            No soporta más este encierro!
+          </p>
+          <p className="game__description--p">
+            Ayer compró unos billetes de avión para escapar de su cautiverio, lejos, muy lejos, pero no consigue
+            descifrar la clave secreta que abre esa puerta y el avión despega en unas horas.
+          </p>
+          <p className="game__description--p bold">Rocky necesita tu ayuda!</p>
+          <p className="game__description--p">
+            Tienes 60 minutos para averiguar todas las pistas que te llevarán a descubrir la clave, y abrir la puerta
+            para que Rocky pueda escapar.
+          </p>
+          <p className="game__description--p bold">Free Rocky!</p>
         </section>
       </main>
       <Footer />
