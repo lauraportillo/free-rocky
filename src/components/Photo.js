506a32bf4ac463cf0f33e241f1001164f9c3dab9
Laura Portillo <lauraportillo.ark@gmail.com>
2021-08-17 16:01:23 +0200

    adjust className in compo photo

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -14,6 +14,7 @@ import '../stylesheets/Photo.scss';
 
 const Photo = (props) => {
   const handleClick = (ev) => {
+    // ev.currentTarget();
     props.handleClick();
   };
   let password01 = Array.from(props.password[0]);
@@ -22,53 +23,53 @@ const Photo = (props) => {
     <>
       <section className="containerPhoto">
         <div className="row1">
-          <div className="divPadre1">
-            <img className="divHijo1" src={one} alt="piece of clue" onClick={handleClick} />
-            <h3 className="divHijo2">{props.showLetter ? password01[0] : null}</h3>
+          <div className="column1">
+            <img className="column__img" src={one} alt="piece of clue" onClick={handleClick} />
+            <h3 className="column__letter">{props.showLetter ? password01[0] : null}</h3>
           </div>
 
-          <div className="divPadre2">
-            <img className="divHijo1" src={two} alt="piece of clue" onClick={handleClick} />
-            <h3 className="divHijo2">{props.showLetter ? password01[1] : null}</h3>
+          <div className="column2">
+            <img className="column__img" src={two} alt="piece of clue" onClick={handleClick} />
+            <h3 className="column__letter">{props.showLetter ? password01[1] : null}</h3>
           </div>
 
-          <div className="divPadre3">
-            <img className="divHijo1" src={three} alt="piece of clue" onClick={handleClick} />
-            <h3 className="divHijo2">{props.showLetter ? password01[2] : null}</h3>
+          <div className="column3">
+            <img className="column__img" src={three} alt="piece of clue" onClick={handleClick} />
+            <h3 className="column__letter">{props.showLetter ? password01[2] : null}</h3>
           </div>
         </div>
 
         <div className="row2">
-          <div className="divPadre1">
-            <img className="divHijo1" src={four} alt="piece of clue" onClick={handleClick} />
-            <h3 className="divHijo2">{props.showLetter ? password01[3] : null}</h3>
+          <div className="column1">
+            <img className="column__img" src={four} alt="piece of clue" onClick={handleClick} />
+            <h3 className="column__letter">{props.showLetter ? password01[3] : null}</h3>
           </div>
 
-          <div className="divPadre2">
-            <img className="divHijo1" src={five} alt="piece of clue" onClick={handleClick} />
-            <h3 className="divHijo2">{props.showLetter ? password01[4] : null}</h3>
+          <div className="column2">
+            <img className="column__img" src={five} alt="piece of clue" onClick={handleClick} />
+            <h3 className="column__letter">{props.showLetter ? password01[4] : null}</h3>
           </div>
 
-          <div className="divPadre3">
-            <img className="divHijo1" src={six} alt="piece of clue" onClick={handleClick} />
-            <h3 className="divHijo2">{props.showLetter ? password01[5] : null}</h3>
+          <div className="column3">
+            <img className="column__img" src={six} alt="piece of clue" onClick={handleClick} />
+            <h3 className="column__letter">{props.showLetter ? password01[5] : null}</h3>
           </div>
         </div>
 
         <div className="row3">
-          <div className="divPadre1">
-            <img className="divHijo1" src={seven} alt="piece of clue" onClick={handleClick} />
-            <h3 className="divHijo2">{props.showLetter ? password01[6] : null}</h3>
+          <div className="column1">
+            <img className="column__img" src={seven} alt="piece of clue" onClick={handleClick} />
+            <h3 className="column__letter">{props.showLetter ? password01[6] : null}</h3>
           </div>
 
-          <div className="divPadre2">
-            <img className="divHijo1" src={eight} alt="piece of clue" onClick={handleClick} />
-            <h3 className="divHijo2">{props.showLetter ? password01[7] : null}</h3>
+          <div className="column2">
+            <img className="column__img" src={eight} alt="piece of clue" onClick={handleClick} />
+            <h3 className="column__letter">{props.showLetter ? password01[7] : null}</h3>
           </div>
 
-          <div className="divPadre3">
-            <img className="divHijo1" src={nine} alt="piece of clue" onClick={handleClick} />
-            <h3 className="divHijo2">{props.showLetter ? password01[8] : null}</h3>
+          <div className="column3">
+            <img className="column__img" src={nine} alt="piece of clue" onClick={handleClick} />
+            <h3 className="column__letter">{props.showLetter ? password01[8] : null}</h3>
           </div>
         </div>
       </section>
