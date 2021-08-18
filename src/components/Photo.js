16a75933a45ac8936399fa851f428819ed73711b
Laura Portillo <lauraportillo.ark@gmail.com>
2021-08-18 13:07:28 +0200

    clean code

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -6,8 +6,6 @@ const Photo = (props) => {
     props.handlePhoto(props.photo.id);
   };
 
-  // const password01 = Array.from(props.password[0]);
-
   return (
     <>
       <div className="photoPiece" id={props.photo.id} onClick={handleClick}>
@@ -19,56 +17,3 @@ const Photo = (props) => {
 };
 
 export default Photo;
-
-// <section className="containerPhoto">
-//     <div className="row1">
-//       <div className="column1">
-//         <img className="column__img" src={props.photos[0].image} alt="piece of clue" onClick={handleClick} />
-//         <h3 className="column__letter">{props.showLetter ? password01[0] : null}</h3>
-//       </div>
-
-//       <div className="column2">
-//         <img className="column__img" src={props.photos[1].image} alt="piece of clue" onClick={handleClick} />
-//         <h3 className="column__letter">{props.showLetter ? password01[1] : null}</h3>
-//       </div>
-
-//       <div className="column3">
-//         <img className="column__img" src={props.photos[2].image} alt="piece of clue" onClick={handleClick} />
-//         <h3 className="column__letter">{props.showLetter ? password01[2] : null}</h3>
-//       </div>
-//     </div>
-
-//     <div className="row2">
-//       <div className="column1">
-//         <img className="column__img" src={props.photos[3].image} alt="piece of clue" onClick={handleClick} />
-//         <h3 className="column__letter">{props.showLetter ? password01[3] : null}</h3>
-//       </div>
-
-//       <div className="column2">
-//         <img className="column__img" src={props.photos[4].image} alt="piece of clue" onClick={handleClick} />
-//         <h3 className="column__letter">{props.showLetter ? password01[4] : null}</h3>
-//       </div>
-
-//       <div className="column3">
-//         <img className="column__img" src={props.photos[5].image} alt="piece of clue" onClick={handleClick} />
-//         <h3 className="column__letter">{props.showLetter ? password01[5] : null}</h3>
-//       </div>
-//     </div>
-
-//     <div className="row3">
-//       <div className="column1">
-//         <img className="column__img" src={props.photos[6].image} alt="piece of clue" onClick={handleClick} />
-//         <h3 className="column__letter">{props.showLetter ? password01[6] : null}</h3>
-//       </div>
-
-//       <div className="column2">
-//         <img className="column__img" src={props.photos[7].image} alt="piece of clue" onClick={handleClick} />
-//         <h3 className="column__letter">{props.showLetter ? password01[7] : null}</h3>
-//       </div>
-
-//       <div className="column3">
-//         <img className="column__img" src={props.photos[8].image} alt="piece of clue" onClick={handleClick} />
-//         <h3 className="column__letter">{props.showLetter ? password01[8] : null}</h3>
-//       </div>
-//     </div>
-//   </section>
