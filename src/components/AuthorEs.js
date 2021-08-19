8341619e7164b29516525522b33396f100aa2753
Laura Portillo <lauraportillo.ark@gmail.com>
2021-08-20 00:46:26 +0200

    add spanish author text

--- src/components/AuthorEs.js
+++ src/components/AuthorEs.js
@@ -15,7 +15,17 @@ const AuthorEs = (props) => {
 
         <section className="author__description">
           <p className="author__description--p">
-            <span className="bold">Castellano:</span> Texto en castellano
+            Hola! Mi nombre es Laura y soy <span className="bold">Front-end Developer</span> junior. Estoy desarrollando
+            este Escape room para seguir practicando y mejorando mis habilidades como programadora. El juego está hecho
+            con React.js, una Librería del lenguaje de programación JavaScript que me encanta y con la que me gustaría
+            seguir trabajando en mis siguientes pasos como developer. El mundo del código me resulta apasionante
+            🚀🚀🚀🚀 También he utilizado el preprocesador Sass para los estilos, usando variables, anidaciones,
+            mixins... reduciendo con ello líneas de código. He usado Git y he hecho muchas ramas para ir probando
+            funcionalidades antes de mergear con la rama máster. Puedes ver el repositorio de GitHub en este enlace:
+            https://github.com/lauraportillo/free-rocky Sugerencias o comentarios para Free Rocky son siempre
+            bienvenidos! Si te ha gustado, comparte para que más gente conozca la historia de Rocky. Y si quisieras
+            invitarme a un café porque realmente has disfrutado con el juego y te gustaría echarme una mano para seguir
+            adelante con mi sueño pincha aquí 👇 y mil gracias de antemano:
           </p>
         </section>
       </main>
