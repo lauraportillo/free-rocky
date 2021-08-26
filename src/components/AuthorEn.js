78ae49f45a7f4b12e3539ba08158f33cd86b8aad
Laura Portillo <lauraportillo.ark@gmail.com>
2021-08-26 12:51:33 +0200

    separate paragraphs in author english component

--- src/components/AuthorEn.js
+++ src/components/AuthorEn.js
@@ -19,17 +19,27 @@ const AuthorEn = (props) => {
         <Languages languageEn={props.languages[4]} languageEs={props.languages[5]} />
 
         <section className="author__description">
+          <p className="author__description--p">Hi there! </p>
           <p className="author__description--p">
-            Hi there! My name is Laura and I am a junior <span className="bold">Front-end Developer</span>. I am
-            developing an Escape room to continue practicing and improving my skills as a developer. The game is made
-            with React.js, a JavaScript programming language library that I love and with which I would like to continue
-            working in my next steps as a developer. Code world is exciting 🚀🚀🚀🚀 I have also used the Sass
-            preprocessor for styles, using variables, nesting, mixins ... reducing lines of code. I have used Git and
-            made many branches to test functionalities before merging with the master branch. GitHub repository here:
-            https://github.com/lauraportillo/free-rocky Suggestions or comments for Free Rocky are always welcome! If
-            you liked it, share it with your friends and more people will know Rocky story. And if you would like to
-            invite me to a coffee because you have really enjoyed the game and would like to give me a hand to continue
-            with my dream, click here 👇 and thank you in advance:
+            My name is Laura and I am a junior <span className="bold">Front-end Developer</span>. I am developing an
+            Escape room to continue practicing and improving my skills as a developer.
+          </p>
+          <p className="author__description--p">
+            The game is made with React.js, a JavaScript programming language library that I love and with which I would
+            like to continue working in my next steps as a developer. Code world is exciting 🚀🚀🚀🚀 I have also used
+            the Sass preprocessor for styles, using variables, nesting, mixins ... reducing lines of code. I have used
+            Git and made many branches to test functionalities before merging with the master branch.
+          </p>
+
+          <p className="author__description--p">GitHub repository here: https://github.com/lauraportillo/free-rocky</p>
+
+          <p className="author__description--p">Suggestions or comments for Free Rocky are always welcome!</p>
+          <p className="author__description--p">
+            If you liked it, share it with your friends and more people will know Rocky story.
+          </p>
+          <p className="author__description--p">
+            And if you would like to invite me to a coffee because you have really enjoyed the game and would like to
+            give me a hand to continue with my dream, thank you in advance and click here 👇
           </p>
         </section>
       </main>
