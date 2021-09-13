38f8c88801108195de0801a3943f4b8b22fbbca9
Laura Portillo <lauraportillo.ark@gmail.com>
2021-09-13 12:19:04 +0200

    add white background to clue 1

--- src/components/Clue01.js
+++ src/components/Clue01.js
@@ -18,39 +18,41 @@ const Clue01 = (props) => {
   return (
     <>
       <ClueHeader />
-      <main className="clue01">
-        <PhotoList password={props.password} photos={props.photos} />
-        <form onSubmit={handleForm} className="clue01__form">
-          <div>
-            <Input
+      <main className="containerClue">
+        <div className="clue01">
+          <PhotoList password={props.password} photos={props.photos} />
+          <form onSubmit={handleForm} className="clue01__form">
+            <div>
+              <Input
+                handleInput={props.handleInput}
+                clue01={props.clue01}
+                className="clue01__form--input"
+                type="text"
+                name="clue01"
+                id="clue01"
+                placeholder="enter the clue..."
+                value={props.clue01}
+              />
+              <ResetButton handleReset={props.handleReset} />
+            </div>
+            <NextButton
               handleInput={props.handleInput}
-              clue01={props.clue01}
-              className="clue01__form--input"
-              type="text"
-              name="clue01"
-              id="clue01"
-              placeholder="enter the clue..."
-              value={props.clue01}
+              input={props.clue01}
+              password={props.password[0]}
+              route={props.route[0]}
             />
-            <ResetButton handleReset={props.handleReset} />
-          </div>
-          <NextButton
-            handleInput={props.handleInput}
-            input={props.clue01}
-            password={props.password[0]}
-            route={props.route[0]}
-          />
-        </form>
+          </form>
 
-        <nav className="clue01Nav">
-          <ul className="clue01Nav__menu">
-            <li>
-              <Link to="/" className="clue01Nav__menu--item">
-                Home
-              </Link>
-            </li>
-          </ul>
-        </nav>
+          <nav className="clue01Nav">
+            <ul className="clue01Nav__menu">
+              <li>
+                <Link to="/" className="clue01Nav__menu--item">
+                  Home
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </main>
       <Footer />
     </>
