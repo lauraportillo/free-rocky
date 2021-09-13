3011aa26f0f2d3cb95c7ebfccbdf442878acfb41
Laura Portillo <lauraportillo.ark@gmail.com>
2021-09-13 13:18:05 +0200

    adjust clue header

--- src/components/ClueHeader.js
+++ src/components/ClueHeader.js
@@ -3,11 +3,13 @@ import '../stylesheets/ClueHeader.scss';
 const ClueHeader = (props) => {
   return (
     <>
-      <header className="clueHeader">
-        <h1 className="clueHeader__title">free rocky!</h1>
-        <div className="clueHeader__time">
-          <h2 className="clueHeader__time--title">time remaining</h2>
-          <div className="clueHeader__time--timer">00.00</div>
+      <header className="clueHeaderContainer">
+        <div className="clueHeader">
+          <h1 className="clueHeader__title">free rocky!</h1>
+          <div className="clueHeader__time">
+            <h2 className="clueHeader__time--title">time remaining</h2>
+            <div className="clueHeader__time--timer">00.00</div>
+          </div>
         </div>
       </header>
     </>
