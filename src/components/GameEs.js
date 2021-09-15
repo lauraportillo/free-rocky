53174d54e6ff0937e1fd3709884229420132faac
Laura Portillo <lauraportillo.ark@gmail.com>
2021-09-15 13:45:59 +0200

    fix spanish game compo

--- src/components/GameEs.js
+++ src/components/GameEs.js
@@ -6,39 +6,41 @@ import Footer from './Footer';
 import '../stylesheets/Game.scss';
 const GameEs = (props) => {
   return (
-    <>
+    <div className="containerGame">
       <Header />
-      <main className="game">
-        <h1 className="game__title">the game</h1>
+      <main className="containerMainGame">
+        <div className="game">
+          <h1 className="game__title">the game</h1>
 
-        <Languages languageEn={props.languages[0]} languageEs={props.languages[1]} />
+          <Languages languageEn={props.languages[0]} languageEs={props.languages[1]} />
 
-        <section className="game__description">
-          <p className="game__description--p">
-            En el año 2020, una pandemia mundial paralizó todo el planeta. Desde entonces, los dueños de Rocky, por
-            miedo a que se contagiara, le han encerrado en una casa abandonada cuya puerta se abre con una clave
-            secreta.
-          </p>
-          <p className="game__description--p">Una vez al día, le traen comida y agua.</p>
-          <p className="game__description--p">
-            Rocky es un perro divertido y dicharachero al que le apasiona viajar por todo el mundo. Le encanta conocer
-            nuevas culturas, hablar diferentes idiomas, charlar con animales salvajes y convivir con humanos amigables…
-            No soporta más este encierro!
-          </p>
-          <p className="game__description--p">
-            Ayer compró unos billetes de avión para escapar de su cautiverio, lejos, muy lejos, pero no consigue
-            descifrar la clave secreta que abre esa puerta y el avión despega en unas horas.
-          </p>
-          <p className="game__description--p bold">Rocky necesita tu ayuda!</p>
-          <p className="game__description--p">
-            Tienes 60 minutos para averiguar todas las pistas que te llevarán a descubrir la clave, y abrir la puerta
-            para que Rocky pueda escapar.
-          </p>
-          <p className="game__description--p bold">Free Rocky!</p>
-        </section>
+          <section className="game__description">
+            <p className="game__description--p">
+              En el año 2020, una pandemia mundial paralizó todo el planeta. Desde entonces, los dueños de Rocky, por
+              miedo a que se contagiara, le han encerrado en una casa abandonada cuya puerta se abre con una clave
+              secreta.
+            </p>
+            <p className="game__description--p">Una vez al día, le traen comida y agua.</p>
+            <p className="game__description--p">
+              Rocky es un perro divertido y dicharachero al que le apasiona viajar por todo el mundo. Le encanta conocer
+              nuevas culturas, hablar diferentes idiomas, charlar con animales salvajes y convivir con humanos
+              amigables… No soporta más este encierro!
+            </p>
+            <p className="game__description--p">
+              Ayer compró unos billetes de avión para escapar de su cautiverio, lejos, muy lejos, pero no consigue
+              descifrar la clave secreta que abre esa puerta y el avión despega en unas horas.
+            </p>
+            <p className="game__description--p bold">Rocky necesita tu ayuda!</p>
+            <p className="game__description--p">
+              Tienes 60 minutos para averiguar todas las pistas que te llevarán a descubrir la clave, y abrir la puerta
+              para que Rocky pueda escapar.
+            </p>
+            <p className="game__description--p bold">Free Rocky!</p>
+          </section>
+        </div>
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
