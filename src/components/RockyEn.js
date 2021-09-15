5a9670cc2359bf027940c12230a0205785a914b2
Laura Portillo <lauraportillo.ark@gmail.com>
2021-09-15 13:55:58 +0200

    fix english rocky component

--- src/components/RockyEn.js
+++ src/components/RockyEn.js
@@ -6,81 +6,83 @@ import Footer from './Footer';
 import '../stylesheets/Rocky.scss';
 const RockyEn = (props) => {
   return (
-    <>
+    <div className="containerRocky">
       <Header />
-      <main className="rocky">
-        <h1 className="rocky__title">rocky dog</h1>
+      <main className="containerMainRocky">
+        <div className="rocky">
+          <h1 className="rocky__title">rocky dog</h1>
 
-        <Languages languageEn={props.languages[2]} languageEs={props.languages[3]} />
+          <Languages languageEn={props.languages[2]} languageEs={props.languages[3]} />
 
-        <section className="rocky__description">
-          <p className="rocky__description--p">
-            Rocky is not a regular dog. He is a very special dog. He is a Bulldog with a beautiful face, like a cuddly
-            teddy bear. The perfect dog that everyone would want to live with.
-          </p>
-          <p className="rocky__description--p">
-            <span className="bold">Rocky</span> seems completely real, however he is not flesh and blood, he is a
-            <span className="bold">3d modelled dog</span>, the work of the great artist
-            <span className="bold">Luis Yrisarry Labadía</span>.
-          </p>
-          <p className="rocky__description--p">
-            Luis is a 3d Artist and Art Director specialized in Characters and Digital Sculpture, with such an
-            incredible talent.
-          </p>
-          <p className="rocky__description--p">
-            Thank you Luis for lend me Rocky as protagonist of this great adventure!
-          </p>
-        </section>
+          <section className="rocky__description">
+            <p className="rocky__description--p">
+              Rocky is not a regular dog. He is a very special dog. He is a Bulldog with a beautiful face, like a cuddly
+              teddy bear. The perfect dog that everyone would want to live with.
+            </p>
+            <p className="rocky__description--p">
+              <span className="bold">Rocky</span> seems completely real, however he is not flesh and blood, he is a
+              <span className="bold">3d modelled dog</span>, the work of the great artist
+              <span className="bold">Luis Yrisarry Labadía</span>.
+            </p>
+            <p className="rocky__description--p">
+              Luis is a 3d Artist and Art Director specialized in Characters and Digital Sculpture, with such an
+              incredible talent.
+            </p>
+            <p className="rocky__description--p">
+              Thank you Luis for lend me Rocky as protagonist of this great adventure!
+            </p>
+          </section>
 
-        <ul className="menu">
-          <li className="menu__item">
-            <a
-              className="menu__item--icon"
-              href="http://bilfrok.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="bilfrok.com"
-            >
-              Luis Yrisarry
-            </a>
-          </li>
-          <li className="menu__item">
-            <a
-              className="menu__item--icon"
-              href="https://www.artstation.com/bilfrok"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="artstation"
-            >
-              <i className="fa fa-artstation" aria-hidden="true"></i>
-            </a>
-          </li>
-          <li className="menu__item">
-            <a
-              className="menu__item--icon"
-              href="https://www.behance.net/Bilfrok"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="behance"
-            >
-              <i className="fab fa-behance" alt="behance icon"></i>
-            </a>
-          </li>
-          <li className="menu__item">
-            <a
-              className="menu__item--icon"
-              href="https://www.linkedin.com/in/bilfrok/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="linkedin"
-            >
-              <i className="fa fa-linkedin" alt="linkedin icon"></i>
-            </a>
-          </li>
-        </ul>
+          <ul className="menu">
+            <li className="menu__item">
+              <a
+                className="menu__item--icon"
+                href="http://bilfrok.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="bilfrok.com"
+              >
+                Luis Yrisarry
+              </a>
+            </li>
+            <li className="menu__item">
+              <a
+                className="menu__item--icon"
+                href="https://www.artstation.com/bilfrok"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="artstation"
+              >
+                <i className="fa fa-artstation" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li className="menu__item">
+              <a
+                className="menu__item--icon"
+                href="https://www.behance.net/Bilfrok"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="behance"
+              >
+                <i className="fab fa-behance" alt="behance icon"></i>
+              </a>
+            </li>
+            <li className="menu__item">
+              <a
+                className="menu__item--icon"
+                href="https://www.linkedin.com/in/bilfrok/"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="linkedin"
+              >
+                <i className="fa fa-linkedin" alt="linkedin icon"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
