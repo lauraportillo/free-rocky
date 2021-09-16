137687f93a901d25eb4f64d8101e3670fe31606d
Laura Portillo <lauraportillo.ark@gmail.com>
2021-09-16 13:22:34 +0200

    fix english author compo

--- src/components/AuthorEn.js
+++ src/components/AuthorEn.js
@@ -7,56 +7,58 @@ import '../stylesheets/Author.scss';
 
 const AuthorEn = (props) => {
   return (
-    <>
+    <div className="containerAuthor">
       <Header />
-      <main className="author">
-        <h1 className="author__title">just me</h1>
+      <main className="containerMainAuthor">
+        <div className="author">
+          <h1 className="author__title">just me</h1>
 
-        <Languages languageEn={props.languages[4]} languageEs={props.languages[5]} />
+          <Languages languageEn={props.languages[4]} languageEs={props.languages[5]} />
 
-        <section className="author__description">
-          <p className="author__description--p">Hi there!</p>
-          <p className="author__description--p">
-            My name is Laura and I am a junior <span className="bold">Front-end Developer</span>. I am developing an
-            Escape Room to continue practicing and improving my skills as a developer.
-          </p>
+          <section className="author__description">
+            <p className="author__description--p">Hi there!</p>
+            <p className="author__description--p">
+              My name is Laura and I am a junior <span className="bold">Front-end Developer</span>. I am developing an
+              Escape Room to continue practicing and improving my skills as a developer.
+            </p>
 
-          <p className="author__description--p">Code world is exciting 🚀🚀🚀🚀</p>
+            <p className="author__description--p">Code world is exciting 🚀🚀🚀🚀</p>
 
-          <p className="author__description--p">
-            The game is made with <span className="bold">React.js</span>, a <span className="bold">JavaScript</span>{' '}
-            programming language library that <span className="bold">I love</span> and with which I would like to
-            continue working in my next steps as a developer. I have also used the <span className="bold">Sass</span>{' '}
-            preprocessor for styles, using variables, nesting, mixins ... reducing lines of code. I have used{' '}
-            <span className="bold">Git</span> and made many branches to test functionalities before merging with the
-            master.
-          </p>
+            <p className="author__description--p">
+              The game is made with <span className="bold">React.js</span>, a <span className="bold">JavaScript</span>{' '}
+              programming language library that <span className="bold">I love</span> and with which I would like to
+              continue working in my next steps as a developer. I have also used the <span className="bold">Sass</span>{' '}
+              preprocessor for styles, using variables, nesting, mixins ... reducing lines of code. I have used{' '}
+              <span className="bold">Git</span> and made many branches to test functionalities before merging with the
+              master.
+            </p>
 
-          <p className="author__description--p link">
-            <a
-              href="https://github.com/lauraportillo/free-rocky"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="github repo"
-            >
-              👉 Click here to visit GitHub repository
-            </a>
-          </p>
+            <p className="author__description--p link">
+              <a
+                href="https://github.com/lauraportillo/free-rocky"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="github repo"
+              >
+                👉 Click here to visit GitHub repository
+              </a>
+            </p>
 
-          <p className="author__description--p">
-            Suggestions or comments for <span className="bold">Free Rocky</span> are always welcome!
-          </p>
-          <p className="author__description--p">
-            If you liked it, share it with your friends and more people will know Rocky story.
-          </p>
-          <p className="author__description--p">
-            And if you would like to invite me to a coffee because you have really enjoyed the game and would like to
-            give me a hand to continue with my dream, thank you in advance and click here 👇
-          </p>
-        </section>
+            <p className="author__description--p">
+              Suggestions or comments for <span className="bold">Free Rocky</span> are always welcome!
+            </p>
+            <p className="author__description--p">
+              If you liked it, share it with your friends and more people will know Rocky story.
+            </p>
+            <p className="author__description--p">
+              And if you would like to invite me to a coffee because you have really enjoyed the game and would like to
+              give me a hand to continue with my dream, thank you in advance and click here 👇
+            </p>
+          </section>
+        </div>
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
