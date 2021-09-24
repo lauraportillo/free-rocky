bd511acec6baa1486b5edcd5e503382effdfdf35
Laura Portillo <lauraportillo.ark@gmail.com>
2021-09-24 12:32:37 +0200

    adjust footer in clue 02

--- src/components/Clue02.js
+++ src/components/Clue02.js
@@ -5,7 +5,7 @@ import ClueHeaderEn from './ClueHeaderEn';
 import Input from './Input';
 import NextButtonEn from './NextButtonEn';
 import ResetButtonEn from './ResetButtonEn';
-import Footer from './Footer';
+import FooterBlack from './FooterBlack';
 // Styles
 import '../stylesheets/Clue02.scss';
 
@@ -15,51 +15,55 @@ const Clue02 = (props) => {
   };
 
   return (
-    <>
+    <div className="containerClue02">
       <ClueHeaderEn />
-      <main className="clue02">
-        <h1 className="clue02__title">segunda</h1>
-        <form onSubmit={handleForm} className="clue02__form">
-          <div>
-            <Input
-              handleInput={props.handleInput}
-              clue02={props.clue02}
-              className="clue02__form--input"
-              type="text"
-              name="clue02"
-              id="clue02"
-              placeholder="enter the clue..."
-              value={props.clue02}
-            />
-            <ResetButtonEn handleReset={props.handleReset} />
+      <main className="containerMainClue02">
+        <div className="clue02">
+          <div className="clue02__text">
+            <h2 className="clue02__title">2 Segunda Pista</h2>
           </div>
+          <form onSubmit={handleForm} className="clue02__form">
+            <div>
+              <Input
+                handleInput={props.handleInput}
+                clue02={props.clue02}
+                className="clue02__form--input"
+                type="text"
+                name="clue02"
+                id="clue02"
+                placeholder="enter the clue..."
+                value={props.clue02}
+              />
+              <ResetButtonEn handleReset={props.handleReset} />
+            </div>
 
-          <NextButtonEn
-            handleInput={props.handleInput}
-            input={props.clue02}
-            password={props.password[1]}
-            route={props.route[1]}
-          />
-        </form>
+            <NextButtonEn
+              handleInput={props.handleInput}
+              input={props.clue02}
+              password={props.password[1]}
+              route={props.route[1]}
+            />
+          </form>
 
-        <nav className="clue02Nav">
-          <ul className="clue02Nav__menu">
-            <li>
-              <Link to="/" className="clue02Nav__menu--item">
-                Home
-              </Link>
-            </li>
-            <li className="clue02Nav__menu--item">|</li>
-            <li>
-              <Link to="/clue01" className="clue02Nav__menu--item">
-                Go back
-              </Link>
-            </li>
-          </ul>
-        </nav>
+          <nav className="clue02Nav">
+            <ul className="clue02Nav__menu">
+              <li>
+                <Link to="/" className="clue02Nav__menu--item">
+                  Home
+                </Link>
+              </li>
+              <li className="clue02Nav__menu--item">|</li>
+              <li>
+                <Link to="/clue01" className="clue02Nav__menu--item">
+                  Go back
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </main>
-      <Footer />
-    </>
+      <FooterBlack />
+    </div>
   );
 };
 
