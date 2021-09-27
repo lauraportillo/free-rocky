d4c5b6987a2b8bb621a8ffdfc6fddfc26ee01994
Laura Portillo <lauraportillo.ark@gmail.com>
2021-09-27 11:22:42 +0200

    add new component for game instructions

---
+++ src/components/InstructionsEn.js
@@ -0,0 +1,14 @@
+// React
+import { Link } from 'react-router-dom';
+// Styles
+import '../stylesheets/Instructions.scss';
+
+const Instructions = (props) => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default Instructions;
