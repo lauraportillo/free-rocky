f6b5bc3535a66d241fbf4d349bc5c1a1339da691
Laura Portillo <lauraportillo.ark@gmail.com>
2021-09-27 12:04:44 +0200

    add instructions text

--- src/components/InstructionsEn.js
+++ src/components/InstructionsEn.js
@@ -6,7 +6,18 @@ import '../stylesheets/Instructions.scss';
 const Instructions = (props) => {
   return (
     <>
-      <div></div>
+      <p>
+        HELP FOR ROCKY EN game instructions You are about to go around the world 🌍 looking for the keys that will make
+        you know the final password to free our friend. Rocky is running out of time to catch his plane and escape from
+        the old mansion where he has been locked up throughout the pandemic. If you are passioned for trips and
+        different cultures, this will be a piece of cake! Some indications that will help you to execute the mission
+        successfully: - You can play from any device - You can play alone or with friends, that depends on you - You can
+        write the keys in both uppercase and lowercase, that will not be a problem - If the password is correct, a
+        message will appear on your screen and it will say "correct" and the "Next" button will be enabled for you to go
+        to the next screen. - I recommend you taking paper and pencil to write notes. Although if you forget something
+        you can always go back and navigate between screens. Hurry up, Rocky is waiting impatiently for your help to
+        open the door! (photo of Rocky running) Get to know a little more about Rocky's history 👉 by clicking here
+      </p>
     </>
   );
 };
