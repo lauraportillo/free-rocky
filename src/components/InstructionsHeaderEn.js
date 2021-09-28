3a22b8c9929237f8664590789e3bd23548ef49e5
Laura Portillo <lauraportillo.ark@gmail.com>
2021-09-28 10:38:35 +0200

    add header and footer to instructions compo

--- src/components/InstructionsHeaderEn.js
+++ src/components/InstructionsHeaderEn.js
@@ -6,10 +6,6 @@ const InstructionsHeaderEn = (props) => {
       <header className="clueHeaderContainer">
         <div className="clueHeader">
           <h1 className="clueHeader__title">free rocky!</h1>
-          <div className="clueHeader__time">
-            <h2 className="clueHeader__time--title">time remaining</h2>
-            <div className="clueHeader__time--timer">00.00</div>
-          </div>
         </div>
       </header>
     </>
