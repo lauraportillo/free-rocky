c885511fea580292bd5f0102c0061155a0966070
Laura Portillo <lauraportillo.ark@gmail.com>
2021-09-30 10:49:49 +0200

    highlight important words instructions english compo

--- src/components/InstructionsEn.js
+++ src/components/InstructionsEn.js
@@ -20,14 +20,17 @@ const InstructionsEn = (props) => {
             <span className="instructions__text--bold"> password</span> to free our friend.
           </p>
           <p className="instructions__text--para">
-            Rocky is running out of time to catch his plane and escape from the old mansion where he has been locked up
+            Rocky is running out of time to catch his plane and
+            <span className="instructions__text--bold"> escape</span> from the old mansion where he has been locked up
             throughout the pandemic.
           </p>
           <p className="instructions__text--para">
-            If you are passioned for trips and different cultures, this will be a piece of cake!
+            If you are passioned for <span className="instructions__text--bold"> trips and different cultures</span> ,
+            this will be a piece of cake!
           </p>
           <p className="instructions__text--para">
-            Some indications that will help you to execute the mission successfully:
+            Some <span className="instructions__text--bold"> indications</span> that will help you to execute the
+            mission successfully:
           </p>
           <p className="instructions__text--para">- You can play from any device </p>
           <p className="instructions__text--para">- You can play alone or with friends, that depends on you</p>
@@ -43,7 +46,8 @@ const InstructionsEn = (props) => {
             back and navigate between screens.
           </p>
           <p className="instructions__text--para">
-            Hurry up, Rocky is waiting impatiently for your help to open the door!
+            Hurry up, Rocky is waiting impatiently for <span className="instructions__text--bold"> your help</span> to
+            open the door!
           </p>
           <p className="instructions__text--para">
             Get to know a little more about Rocky's history 👉 by clicking
