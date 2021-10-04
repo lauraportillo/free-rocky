dff6da27f413b6ec4be686bd95fee55300d15de4
Laura Portillo <lauraportillo.ark@gmail.com>
2021-10-04 10:28:47 +0200

    add new compo for clues to guess temple name

---
+++ src/components/Clue01TempleEn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/Clue01TempleEn.scss';
+
+const Clue01TempleEn = (props) => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default Clue01TempleEn;
