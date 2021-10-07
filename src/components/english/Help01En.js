bd604486e834f44afc52eccb127cf7ba2836d400
Laura Portillo <lauraportillo.ark@gmail.com>
2021-10-07 13:45:07 +0200

    add text to compo help 01

--- src/components/english/Help01En.js
+++ src/components/english/Help01En.js
@@ -7,7 +7,11 @@ import '../../stylesheets/Help01.scss';
 const Help01En = (props) => {
   return (
     <>
-      <p className="help01">esto es una prueba</p>
+      <p className="help01">
+        The temple: CLUE 1: By fitting the pieces of the puzzle you will be able to read the name from left to right and
+        following the lines. CLUE 2: The name of the temple contains the name of the chemical element whose symbol is: B
+        CLUE 3: take a pencil and paper and here you have the puzzle solved.
+      </p>
     </>
   );
 };
