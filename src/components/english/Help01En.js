8abfa3a7ba4794acf8dc91f9a6d15c0e26953b5a
Laura Portillo <lauraportillo.ark@gmail.com>
2021-10-08 10:18:38 +0200

    change help text

--- src/components/english/Help01En.js
+++ src/components/english/Help01En.js
@@ -10,7 +10,8 @@ const Help01En = (props) => {
       <i class="fas fa-paw"></i>
 
       <p className="help01">
-        The temple: CLUE 1: By fitting the pieces of the puzzle you will be able to read the name from left to right and
+        The temple: Do you need help finding out the name of the Temple? Click on the clues you need to find the
+        solution! CLUE 1: By fitting the pieces of the puzzle you will be able to read the name from left to right and
         following the lines. CLUE 2: The name of the temple contains the name of the chemical element whose symbol is: B
         CLUE 3: take a pencil and paper and here you have the puzzle solved.
       </p>
