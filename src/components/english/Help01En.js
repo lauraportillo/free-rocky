d195635cb42fa2ac25b8c18fb53548a777ec8327
Laura Portillo <lauraportillo.ark@gmail.com>
2021-10-08 11:45:19 +0200

    adjust help compo structure

--- src/components/english/Help01En.js
+++ src/components/english/Help01En.js
@@ -1,6 +1,7 @@
 // React
 import React from 'react';
 // Components
+import ClueHeaderEn from './ClueHeaderEn';
 import FooterBlack from '../FooterBlack';
 // Styles
 import '../../stylesheets/Help01.scss';
@@ -8,28 +9,31 @@ import '../../stylesheets/Help01.scss';
 const Help01En = (props) => {
   return (
     <div className="containerHelp01">
-      <header className="headerHelp01">
-        <i class="fas fa-paw"></i>
-        <i class="fas fa-paw"></i>
-        <i class="fas fa-paw"></i>
-        <i class="fas fa-paw"></i>
-      </header>
+      <ClueHeaderEn />
 
       <main className="containerMainHelp01">
         <div className="help01">
-          <h1 className="help01__title">1 The temple</h1>
-          <h2 className="help01__subtitle">Do you need help finding out the name of the Temple?</h2>
-          <h2 className="help01__subtitle">Click on the clues you need to find the solution!</h2>
-          <p className="help01__text">
-            CLUE 1: By fitting the pieces of the puzzle you will be able to read the name from left to right and
-            following the lines.
-          </p>
+          <div className="help01__paws">
+            <i class="fas fa-paw"></i>
+            <i class="fas fa-paw"></i>
+            <i class="fas fa-paw"></i>
+            <i class="fas fa-paw"></i>
+          </div>
+          <div className="help01__text">
+            <h1 className="help01__text--title">1 The temple</h1>
+            <h2 className="help01__text--subtitle">Do you need help finding out the name of the Temple?</h2>
+            <h2 className="help01__text--subtitle">Click on the clues you need to find the solution!</h2>
+            <p className="help01__text--para">
+              CLUE 1: By fitting the pieces of the puzzle you will be able to read the name from left to right and
+              following the lines.
+            </p>
 
-          <p className="help01__text">
-            CLUE 2: The name of the temple contains the name of the chemical element whose symbol is: B
-          </p>
+            <p className="help01__text--para">
+              CLUE 2: The name of the temple contains the name of the chemical element whose symbol is: B
+            </p>
 
-          <p className="help01__text">CLUE 3: take a pencil and paper and here you have the puzzle solved.</p>
+            <p className="help01__text--para">CLUE 3: take a pencil and paper and here you have the puzzle solved.</p>
+          </div>
         </div>
       </main>
 
