58c507dcce412ff47299137ee8e63440cdf50e89
Laura Portillo <lauraportillo.ark@gmail.com>
2021-10-18 11:03:15 +0200

    fix DOM issue

--- src/components/english/Help01Clue4En.js
+++ src/components/english/Help01Clue4En.js
@@ -4,10 +4,10 @@ import '../../stylesheets/Help01Clue4En.scss';
 const Help01Clue4En = () => {
   return (
     <>
-      <div className="help01Clue4">
+      <section className="help01Clue4">
         <p className="help01Clue4__text">Take a pencil and paper and here you have the puzzle solved.</p>
         <div className="help01Clue4__img"></div>
-      </div>
+      </section>
     </>
   );
 };
