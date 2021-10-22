545608bfc2505c70861f8c5e367ec363e89e4df8
Laura Portillo <lauraportillo.ark@gmail.com>
2021-10-22 10:27:21 +0200

    create help01Clue4 spanish

---
+++ src/components/spanish/Help01Clue4Es.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../stylesheets/Help01Clue4En.scss';
+
+const Help01Clue4En = () => {
+  return (
+    <section className="help01Clue4">
+      <p className="help01Clue4__text">Take a pencil and paper and here you have the puzzle solved.</p>
+      <div className="help01Clue4__img"></div>
+    </section>
+  );
+};
+
+export default Help01Clue4En;
