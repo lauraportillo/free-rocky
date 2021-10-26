77f6368fb3b6972bb96c99c6237f029a5446276e
Laura Portillo <lauraportillo.ark@gmail.com>
2021-10-26 12:19:17 +0200

    center puzzle clue 4 clue

--- src/components/spanish/Help01Clue4Es.js
+++ src/components/spanish/Help01Clue4Es.js
@@ -5,7 +5,9 @@ const Help01Clue4En = () => {
   return (
     <section className="help01Clue4">
       <p className="help01Clue4__text">Toma papel y lápiz y aquí tienes el puzzle resuelto.</p>
-      <div className="help01Clue4__img"></div>
+      <div className="help01Clue4__container">
+        <div className="help01Clue4__container--img"></div>
+      </div>
     </section>
   );
 };
