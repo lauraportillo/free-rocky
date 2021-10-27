844a449a89e5c8038b347238349521d442b0dcdc
Laura Portillo <lauraportillo.ark@gmail.com>
2021-10-27 11:01:32 +0200

    create coming soon compo

---
+++ src/components/ComingSoon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/ComingSoon.scss';
+
+const ComingSoon = (props) => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default ComingSoon;
