ffce8e81dfec232e582336201b0f850257abb2f1
Laura Portillo <lauraportillo.ark@gmail.com>
2021-10-27 15:24:47 +0200

    adjust links hover

--- src/components/english/Clue01En.js
+++ src/components/english/Clue01En.js
@@ -33,7 +33,7 @@ const Clue01En = (props) => {
               Find in the images the letters that will lead you to discover the name of the temple.
             </p>
             <p className="clue01__text--para">
-              <Link to="/help01En" className="clue01__text--help ">
+              <Link to="/help01En" className="clue01__text--help">
                 🔎 Clues!
               </Link>
             </p>
