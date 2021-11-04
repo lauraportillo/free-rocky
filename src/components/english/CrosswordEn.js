87f84c9f566a50dfa658123b4146255ee503f3d9
Laura Portillo <lauraportillo.ark@gmail.com>
2021-11-04 11:49:54 +0100

    start the crossword

--- src/components/english/CrosswordEn.js
+++ src/components/english/CrosswordEn.js
@@ -4,7 +4,11 @@ import '../../stylesheets/Crossword.scss';
 const CrosswordEn = (props) => {
   return (
     <>
-      <div className="prueba">probando crucigrama</div>
+      <div className="prueba1">
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="1" />
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="2" />
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="3" />
+      </div>
     </>
   );
 };
