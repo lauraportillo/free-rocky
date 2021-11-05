587e40b551db3993b18cb489e944bdb3df2ad880
Laura Portillo <lauraportillo.ark@gmail.com>
2021-11-05 12:12:56 +0100

    add display grid to container

--- src/components/english/CrosswordEn.js
+++ src/components/english/CrosswordEn.js
@@ -4,20 +4,24 @@ import '../../stylesheets/Crossword.scss';
 const CrosswordEn = (props) => {
   return (
     <>
-      <div className="prueba1">
-        <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
+      <div className="crossword">
+        <input class="crossword__empty" type="text" disabled="disabled" maxlength="1" />
         <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
         <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
         <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
         <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
         <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
         <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="P" />
-        <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="1" />
-        <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="1" />
-        <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="1" />
-        <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="1" />
-        <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="1" />
-        <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="1" />
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
+
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="X" />
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="X" />
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="X" />
       </div>
     </>
   );
