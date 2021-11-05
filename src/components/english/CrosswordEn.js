6bbc6bbe825c3273e5f2ae87b9d83c6c80d7bdce
Laura Portillo <lauraportillo.ark@gmail.com>
2021-11-05 11:53:50 +0100

    add first row

--- src/components/english/CrosswordEn.js
+++ src/components/english/CrosswordEn.js
@@ -5,6 +5,13 @@ const CrosswordEn = (props) => {
   return (
     <>
       <div className="prueba1">
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" />
+        <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="P" />
         <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="1" />
         <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="1" />
         <input class="prueba2" type="text" disabled="disabled" maxlength="1" value="1" />
