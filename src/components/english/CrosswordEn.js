5455e84cf599fc6980c4866d9ba7276821b7b684
Laura Portillo <lauraportillo.ark@gmail.com>
2021-11-05 13:06:05 +0100

    add row 6

--- src/components/english/CrosswordEn.js
+++ src/components/english/CrosswordEn.js
@@ -60,6 +60,34 @@ const CrosswordEn = (props) => {
         <input className="crossword__empty" disabled="disabled" />
         <input className="crossword__empty" disabled="disabled" />
         <input className="crossword__empty" disabled="disabled" />
+
+        <input className="crossword__full" type="text" maxLength="1" value="B" />
+        <input className="crossword__full" type="text" maxLength="1" value="U" />
+        <input className="crossword__full" type="text" maxLength="1" value="D" />
+        <input className="crossword__full" type="text" maxLength="1" value="A" />
+        <input className="crossword__full" type="text" maxLength="1" value="P" />
+        <input className="crossword__full" type="text" maxLength="1" value="E" />
+        <input className="crossword__full" type="text" maxLength="1" value="S" />
+        <input className="crossword__full" type="text" maxLength="1" value="T" />
+        <input className="crossword__empty" disabled="disabled" />
+        <input className="crossword__empty" disabled="disabled" />
+        <input className="crossword__empty" disabled="disabled" />
+        <input className="crossword__empty" disabled="disabled" />
+        <input className="crossword__empty" disabled="disabled" />
+
+        <input className="crossword__full" type="text" maxLength="1" value="E" />
+        <input className="crossword__empty" disabled="disabled" />
+        <input className="crossword__empty" disabled="disabled" />
+        <input className="crossword__full" type="text" maxLength="1" value="S" />
+        <input className="crossword__empty" disabled="disabled" />
+        <input className="crossword__empty" disabled="disabled" />
+        <input className="crossword__empty" disabled="disabled" />
+        <input className="crossword__full" type="text" maxLength="1" value="I" />
+        <input className="crossword__empty" disabled="disabled" />
+        <input className="crossword__empty" disabled="disabled" />
+        <input className="crossword__empty" disabled="disabled" />
+        <input className="crossword__empty" disabled="disabled" />
+        <input className="crossword__empty" disabled="disabled" />
       </div>
     </>
   );
