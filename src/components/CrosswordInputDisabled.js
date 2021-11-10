ed42f5aa13d981a3307b5a933ba71759296856e6
Laura Portillo <lauraportillo.ark@gmail.com>
2021-11-10 15:28:19 +0100

    add new component CrosswordInputDisabled

---
+++ src/components/CrosswordInputDisabled.js
@@ -0,0 +1,9 @@
+const CrosswordInputDisabled = (props) => {
+  return (
+    <>
+      <input className="crossword__empty" disabled="disabled" />
+    </>
+  );
+};
+
+export default CrosswordInputDisabled;
