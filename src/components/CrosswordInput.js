33d6fcc88b7bbb8dde0a5a53055e3ea67227ac05
Laura Portillo <lauraportillo.ark@gmail.com>
2021-11-13 10:47:54 +0100

    create const to keep a complete word

--- src/components/CrosswordInput.js
+++ src/components/CrosswordInput.js
@@ -2,12 +2,15 @@ const CrosswordInput = (props) => {
   const handleChange = (ev) => {
     props.handleCrosswordInput(ev.target.name, ev.target.value);
   };
+
+  const madrid = props.md01 + props.md02 + props.md03 + props.md04 + props.md05 + props.md06;
+
   return (
     <>
       <input
         onChange={handleChange}
         // className="crossword__full"
-        className={props.md01 === 'M' ? 'crossword__correct' : 'crossword__full'}
+        className={madrid === 'MADRID' ? 'crossword__correct' : 'crossword__full'}
         type="text"
         maxLength="1"
         name={props.name}
