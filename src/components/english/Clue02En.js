b4ec136377b0b24b74eafca9b5bd6434ee05be0f
Laura Portillo <lauraportillo.ark@gmail.com>
2021-11-19 11:47:22 +0100

    add down one

--- src/components/english/Clue02En.js
+++ src/components/english/Clue02En.js
@@ -106,7 +106,7 @@ const Clue02En = (props) => {
             <p className="clue02__text--para2">4 Luzon island’s second most populous city.</p>
 
             <h3 className="clue02__text--para1">Down</h3>
-            <p className="clue02__text--para2">1</p>
+            <p className="clue02__text--para2">1 One of the most famous walls in the world.</p>
             <p className="clue02__text--para2">2</p>
             <p className="clue02__text--para2">3</p>
             <p className="clue02__text--para2">4</p>
