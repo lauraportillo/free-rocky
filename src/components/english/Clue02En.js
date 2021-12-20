f6c593cb0e8e42545bf4d6e34731cdc3bb2de639
Laura Portillo <lauraportillo.ark@gmail.com>
2021-12-20 13:46:51 +0100

    fix icons bold problem

--- src/components/english/Clue02En.js
+++ src/components/english/Clue02En.js
@@ -257,15 +257,15 @@ const Clue02En = (props) => {
               is, go ahead! Otherwise, solve this crossword and follow the clues!
             </p>
 
-            <h3 className="clue02__text--para1">Cities of the world:</h3>
+            <h3 className="clue02__text--para1">Cities of the world</h3>
 
-            <h3 className="clue02__text--para1">Across</h3>
+            <p className="clue02__text--para1">Across:</p>
             <p className="clue02__text--para2">{city[1].clue}</p>
             <p className="clue02__text--para2">{city[0].clue}</p>
             <p className="clue02__text--para2">{city[2].clue}</p>
             <p className="clue02__text--para2">{city[3].clue}</p>
 
-            <h3 className="clue02__text--para1">Down</h3>
+            <p className="clue02__text--para1">Down:</p>
             <p className="clue02__text--para2">{city[4].clue}</p>
             <p className="clue02__text--para2">{city[5].clue}</p>
             <p className="clue02__text--para2">{city[6].clue}</p>
@@ -273,14 +273,20 @@ const Clue02En = (props) => {
           </section>
 
           <section className="clue02__text">
-            <p className="clue02__text--para1">
-              Next step 👉 You have solved it easily, right? Now take a good look at everything that the crossword shows
-              to you, because it hides the postal code of the museum's location. Find out the following code: 5 + 6 +
-              number of W + 7 + 8 + number of A
+            <p className="clue02__text--para2">
+              <span className="clue02__text--paraBoldBlack">Next step</span> 👉
             </p>
+
+            <p className="clue02__text--para1">You have solved it easily, right?</p>
             <p className="clue02__text--para1">
-              <Link to="/help02En" className="clue01__text--help">
-                🔎 Clues!
+              Now take a good look at everything that the crossword shows to you, because it hides the postal code of
+              the museum's location.
+            </p>
+            <p className="clue02__text--para1">Find out the following code:</p>
+            <p className="clue02__text--para1">5 + 6 + number of W + 7 + 8 + number of A</p>
+            <p className="clue02__text--help">
+              <Link to="/help02En">
+                🔎 <span className="clue02__text--paraBoldGray">Clues!</span>
               </Link>
             </p>
           </section>
